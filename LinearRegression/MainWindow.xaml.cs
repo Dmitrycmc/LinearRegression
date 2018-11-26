c1a22410a87d9b9fcdf5a4e66aa2a756debeecfc
Dmitry Litov <Dmitrycmc@gmail.com>
2018-11-26 07:29:04 +0300

    Incapsulated

--- LinearRegression/MainWindow.xaml.cs
+++ LinearRegression/MainWindow.xaml.cs
@@ -1,21 +1,5 @@
-﻿using Microsoft.ML.Probabilistic.Algorithms;
-using Microsoft.ML.Probabilistic.Distributions;
-using Microsoft.ML.Probabilistic.Math;
-using Microsoft.ML.Probabilistic.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LinearRegression
 {
@@ -34,31 +18,22 @@ namespace LinearRegression
 		{
 			try
 			{
-				double realA = Rand.UniformBetween(-2, 2);
-				double realB = Rand.UniformBetween(-20, 20);
+				Model model = new Model(10);
+				string res = model.infer();
 
 
-				double[] data = new double[100];
-				for (int i = 0; i < data.Length; i++) data[i] = realA * i + realB + Rand.Normal(0, 1);
-				
-				Variable<double> a = Variable.GaussianFromMeanAndVariance(0, 100).Named("a");
-				Variable<double> b = Variable.GaussianFromMeanAndVariance(0, 100).Named("b");
-				Variable<double> precision = Variable.GammaFromShapeAndScale(1, 1).Named("precision");
 
-				Range dataRange = new Range(data.Length);
-				VariableArray<double> x = Variable.Array<double>(dataRange);
-				for (int i = 0; i < data.Length; i++)
-				{
-					x[i] = a * i + b + Variable.GaussianFromMeanAndPrecision(0, precision);
-				}
 
-				x.ObservedValue = data;
 
-				// Create an inference engine for VMP  
-				InferenceEngine engine = new InferenceEngine(new ExpectationPropagation());
-				MessageBox.Show("precision: " + engine.Infer(precision));
-				MessageBox.Show("B: " + engine.Infer(b) + " real: " + realB);
-				MessageBox.Show("A: " + engine.Infer(a) + " real: " + realA);
+				MessageBox.Show(res);
+				MessageBox.Show("A: " + model.A + " " + model.predictedA);
+				MessageBox.Show("B: " + model.B + " " + model.predictedB);
+				MessageBox.Show("Var: " + model.Var + " " + model.predictedVar);
+
+
+
+
+
 			}
 			catch (Exception exception)
 			{
---
+++ LinearRegression/Model.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.ML.Probabilistic.Algorithms;
+using Microsoft.ML.Probabilistic.Math;
+using Microsoft.ML.Probabilistic.Models;
+using System;
+using System.Windows;
+
+namespace LinearRegression
+{
+	class Model
+	{
+		/* real */
+		public double A { get; }
+		public double B { get; }
+		public double Var { get; }
+		/* predicted */
+		private double a;
+		public double predictedA { get { return a; } }
+		private double b;
+		public double predictedB { get { return a; } }
+		private double var;
+		public double predictedVar { get { return a; } }
+		
+		public double[] data { get; }
+
+		public Model(int N = 100)
+		{
+			A = Rand.UniformBetween(-2, 2);
+			B = Rand.UniformBetween(-20, 20);
+			Var = Rand.UniformBetween(1, 10);
+
+			data = new double[N];
+			for (int i = 0; i < data.Length; i++) data[i] = A * i + B + Rand.Normal(0, Var);
+		}
+
+		private string trimEnd(string str)
+		{
+			while (!char.IsNumber(str[str.Length - 1])) str = str.Substring(0, str.Length - 1);
+			return str;
+		}
+
+		private double parseBetween(string str, char c1, char c2)
+		{
+			int firstSign = str.IndexOf(c1) + 1;
+			int lastSign = str.IndexOf(c2);
+			string substr = str.Substring(firstSign, lastSign - firstSign);
+			substr = trimEnd(substr);
+			double res = double.Parse(substr);
+			return res;
+		}
+
+		public string infer()
+		{
+			Variable<double> a = Variable.GaussianFromMeanAndVariance(0, 100).Named("a");
+			Variable<double> b = Variable.GaussianFromMeanAndVariance(0, 100).Named("b");
+			Variable<double> precision = Variable.GammaFromShapeAndScale(1, 1).Named("precision");
+
+			Range dataRange = new Range(data.Length);
+			VariableArray<double> y = Variable.Array<double>(dataRange);
+			for (int i = 0; i < data.Length; i++)
+			{
+				y[i] = a * i + b + Variable.GaussianFromMeanAndPrecision(0, precision);
+			}
+
+			y.ObservedValue = data;
+			
+			InferenceEngine engine = new InferenceEngine(new ExpectationPropagation());
+			
+			string precisionString = engine.Infer(precision).ToString();
+			string aString = engine.Infer(a).ToString();
+			string bString = engine.Infer(b).ToString();
+
+			string ans = "Precision: " + precisionString + Environment.NewLine;
+			ans += "A: " + aString + Environment.NewLine;
+			ans += "B: " + bString;
+
+			this.var = (double)1 / parseBetween(precisionString, '=', ']');
+			this.a = parseBetween(aString, '(', ' ');
+			this.b = parseBetween(bString, '(', ' ');
+
+			return ans;
+		}
+	}
+}
